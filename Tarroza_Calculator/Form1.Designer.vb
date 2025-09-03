b5d8645132cfc2a4e97ea9c35b36039888c7a129
TheEliteSquad25 <tarrozanathaniel25@gmail.com>
2025-09-03 19:29:18 +0800

    Add project files.

---
+++ Tarroza_Calculator/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnAC = New Button()
+        txtDisplay = New TextBox()
+        btnBackspace = New Button()
+        btnDivide = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btnMultiply = New Button()
+        btn4 = New Button()
+        btn1 = New Button()
+        btn5 = New Button()
+        btn2 = New Button()
+        btn0 = New Button()
+        btn6 = New Button()
+        btnDecimal = New Button()
+        btn3 = New Button()
+        btnEquals = New Button()
+        btnSubtract = New Button()
+        btnAdd = New Button()
+        SuspendLayout()
+        ' 
+        ' btnAC
+        ' 
+        btnAC.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAC.Location = New Point(7, 96)
+        btnAC.Name = "btnAC"
+        btnAC.Size = New Size(146, 50)
+        btnAC.TabIndex = 0
+        btnAC.Text = "AC"
+        btnAC.UseVisualStyleBackColor = True
+        ' 
+        ' txtDisplay
+        ' 
+        txtDisplay.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        txtDisplay.Location = New Point(7, 10)
+        txtDisplay.Multiline = True
+        txtDisplay.Name = "txtDisplay"
+        txtDisplay.ReadOnly = True
+        txtDisplay.Size = New Size(290, 80)
+        txtDisplay.TabIndex = 1
+        txtDisplay.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' btnBackspace
+        ' 
+        btnBackspace.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnBackspace.Location = New Point(153, 96)
+        btnBackspace.Name = "btnBackspace"
+        btnBackspace.Size = New Size(72, 50)
+        btnBackspace.TabIndex = 2
+        btnBackspace.Text = "⌫"
+        btnBackspace.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDivide.Location = New Point(225, 96)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(72, 50)
+        btnDivide.TabIndex = 3
+        btnDivide.Text = "/"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn7.Location = New Point(7, 152)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(73, 50)
+        btn7.TabIndex = 4
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn8.Location = New Point(80, 152)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(73, 50)
+        btn8.TabIndex = 5
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn9.Location = New Point(153, 152)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(72, 50)
+        btn9.TabIndex = 6
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnMultiply.Location = New Point(225, 152)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(72, 50)
+        btnMultiply.TabIndex = 7
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn4.Location = New Point(7, 208)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(73, 50)
+        btn4.TabIndex = 8
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn1.Location = New Point(7, 264)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(73, 50)
+        btn1.TabIndex = 9
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn5.Location = New Point(80, 208)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(73, 50)
+        btn5.TabIndex = 11
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn2.Location = New Point(80, 264)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(73, 50)
+        btn2.TabIndex = 12
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn0.Location = New Point(7, 320)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(146, 50)
+        btn0.TabIndex = 13
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn6.Location = New Point(153, 208)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(72, 50)
+        btn6.TabIndex = 14
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btnDecimal
+        ' 
+        btnDecimal.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDecimal.Location = New Point(153, 320)
+        btnDecimal.Name = "btnDecimal"
+        btnDecimal.Size = New Size(72, 50)
+        btnDecimal.TabIndex = 15
+        btnDecimal.Text = "."
+        btnDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn3.Location = New Point(153, 264)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(72, 50)
+        btn3.TabIndex = 15
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnEquals.Location = New Point(225, 320)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(72, 50)
+        btnEquals.TabIndex = 16
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnSubtract.Location = New Point(225, 208)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(72, 50)
+        btnSubtract.TabIndex = 16
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(225, 264)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(72, 50)
+        btnAdd.TabIndex = 17
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AcceptButton = btnEquals
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DeepSkyBlue
+        ClientSize = New Size(304, 381)
+        Controls.Add(btnAdd)
+        Controls.Add(btnSubtract)
+        Controls.Add(btnEquals)
+        Controls.Add(btn3)
+        Controls.Add(btnDecimal)
+        Controls.Add(btn6)
+        Controls.Add(btn0)
+        Controls.Add(btn2)
+        Controls.Add(btn5)
+        Controls.Add(btn1)
+        Controls.Add(btn4)
+        Controls.Add(btnMultiply)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btnDivide)
+        Controls.Add(btnBackspace)
+        Controls.Add(txtDisplay)
+        Controls.Add(btnAC)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        KeyPreview = True
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnAC As Button
+    Friend WithEvents txtDisplay As TextBox
+    Friend WithEvents btnBackspace As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnAdd As Button
+
+End Class
---
+++ Tarroza_Calculator/Form1.vb
@@ -0,0 +1,180 @@
+﻿Public Class Form1
+    ' === Variables ===
+    Dim firstNumber As Double = 0
+    Dim secondNumber As Double = 0
+    Dim operation As String = ""
+    Dim resultCalculated As Boolean = False
+    Dim lastInputIsNumber As Boolean = False
+    Dim lastInputIsOperator As Boolean = False ' NEW
+
+    ' === Form Load (enable keyboard support) ===
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.KeyPreview = True
+    End Sub
+
+    ' === Number and Decimal Buttons (0–9, .) ===
+    Private Sub btnNumber_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click,
+        btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click, btnDecimal.Click
+
+        Dim btn As Button = CType(sender, Button)
+
+        ' Prevent multiple decimals in the current number
+        If btn.Text = "." Then
+            Dim parts() As String = txtDisplay.Text.Split(" "c)
+            Dim currentPart As String = If(parts.Length > 0, parts(parts.Length - 1), "")
+            If currentPart.Contains(".") OrElse currentPart = "" Then Exit Sub
+        End If
+
+        txtDisplay.Text &= btn.Text
+        lastInputIsNumber = True
+        lastInputIsOperator = False
+        resultCalculated = False ' Always allow chaining/appending
+    End Sub
+
+    ' === Operator Buttons (+ - * /) ===
+    Private Sub btnOperator_Click(sender As Object, e As EventArgs) Handles btnAdd.Click, btnSubtract.Click, btnMultiply.Click, btnDivide.Click
+        Dim btn As Button = CType(sender, Button)
+
+        If txtDisplay.Text = "" OrElse Not lastInputIsNumber Then
+            MessageBox.Show("Operator must follow a number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' If first number, operator, AND second number exist, calculate first
+        Dim parts() As String = txtDisplay.Text.Split(" "c)
+        If parts.Length = 3 Then
+            btnEquals.PerformClick()
+            parts = txtDisplay.Text.Split(" "c)
+            If txtDisplay.Text = "" Then Exit Sub
+        End If
+
+        operation = btn.Text
+        txtDisplay.Text &= " " & operation & " "
+        lastInputIsNumber = False
+        lastInputIsOperator = True
+        resultCalculated = False ' Allow chaining
+    End Sub
+
+    ' Helper to normalize display for parsing
+    Private Function NormalizeDisplay(text As String) As String
+        ' Add spaces around operators for consistent splitting
+        Dim ops As String() = {"+", "-", "*", "/"}
+        For Each op In ops
+            text = text.Replace(op, " " & op & " ")
+        Next
+        ' Replace multiple spaces with single space
+        Return System.Text.RegularExpressions.Regex.Replace(text, "\s+", " ").Trim()
+    End Function
+
+    ' === Equals Button (=) ===
+    Private Sub btnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        ' Only proceed if the display contains a valid expression: number operator number
+        Dim normalized As String = NormalizeDisplay(txtDisplay.Text)
+        Dim parts() As String = normalized.Split(" "c)
+        If parts.Length < 3 Then Exit Sub
+
+        Dim op As String = parts(1)
+        Dim num1 As Double, num2 As Double
+
+        If Not Double.TryParse(parts(0), num1) Then Exit Sub
+        If Not Double.TryParse(parts(2), num2) Then Exit Sub
+
+        Dim result As Double
+
+        Try
+            Select Case op
+                Case "+" : result = num1 + num2
+                Case "-" : result = num1 - num2
+                Case "*" : result = num1 * num2
+                Case "/"
+                    If num2 = 0 Then
+                        MessageBox.Show("Cannot divide by zero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                        Exit Sub
+                    End If
+                    result = num1 / num2
+                Case Else
+                    Exit Sub
+            End Select
+
+            txtDisplay.Text = FormatResult(result)
+            firstNumber = result
+            resultCalculated = False ' Allow appending after result
+            operation = ""
+            lastInputIsNumber = True
+            lastInputIsOperator = False
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Helper to format result, trimming trailing zeros
+    Private Function FormatResult(value As Double) As String
+        Dim formatted As String = value.ToString("0.##########")
+        Return formatted
+    End Function
+
+    ' === All Clear (AC) ===
+    Private Sub btnAC_Click(sender As Object, e As EventArgs) Handles btnAC.Click
+        txtDisplay.Clear()
+        firstNumber = 0
+        secondNumber = 0
+        operation = ""
+        resultCalculated = False
+        lastInputIsNumber = False
+    End Sub
+
+    ' === Backspace (⌫) ===
+    Private Sub btnBackspace_Click(sender As Object, e As EventArgs) Handles btnBackspace.Click
+        If txtDisplay.Text.Length > 0 Then
+            txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1)
+            If txtDisplay.Text.Length > 0 Then
+                Dim lastChar As Char = txtDisplay.Text(txtDisplay.Text.Length - 1)
+                lastInputIsNumber = Char.IsDigit(lastChar) Or lastChar = "."
+            Else
+                lastInputIsNumber = False
+            End If
+        End If
+    End Sub
+
+    ' === Keyboard Input Support ===
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyCode
+            ' Numbers
+            Case Keys.D0, Keys.NumPad0 : btn0.PerformClick()
+            Case Keys.D1, Keys.NumPad1 : btn1.PerformClick()
+            Case Keys.D2, Keys.NumPad2 : btn2.PerformClick()
+            Case Keys.D3, Keys.NumPad3 : btn3.PerformClick()
+            Case Keys.D4, Keys.NumPad4 : btn4.PerformClick()
+            Case Keys.D5, Keys.NumPad5 : btn5.PerformClick()
+            Case Keys.D6, Keys.NumPad6 : btn6.PerformClick()
+            Case Keys.D7, Keys.NumPad7 : btn7.PerformClick()
+            Case Keys.D8
+                If e.Shift Then
+                    btnMultiply.PerformClick() ' Shift+8 is *
+                Else
+                    btn8.PerformClick()
+                End If
+            Case Keys.NumPad8 : btn8.PerformClick()
+            Case Keys.D9, Keys.NumPad9 : btn9.PerformClick()
+
+            ' Decimal
+            Case Keys.Decimal, Keys.OemPeriod : btnDecimal.PerformClick()
+
+            ' Operators
+            Case Keys.Add, Keys.Oemplus : btnAdd.PerformClick()
+            Case Keys.Subtract, Keys.OemMinus : btnSubtract.PerformClick()
+            Case Keys.Multiply : btnMultiply.PerformClick() ' Numpad *
+            Case Keys.Divide, Keys.OemQuestion : btnDivide.PerformClick()
+
+            ' Equals (Enter/Return)
+            Case Keys.Enter, Keys.Return
+                btnEquals.PerformClick()
+                e.Handled = True
+                e.SuppressKeyPress = True
+
+            ' Clear and Backspace
+            Case Keys.Delete : btnAC.PerformClick()
+            Case Keys.Back : btnBackspace.PerformClick()
+        End Select
+    End Sub
+End Class
